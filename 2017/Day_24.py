0dc663643da62e9b26e1889d7415f9363ef846a3
DSW41923 <dsw41923@gmail.com>
2023-03-18 14:44:15 +0800

    Complete Day 23, 24, 25

--- 2017/Day_24.py
+++ 2017/Day_24.py
@@ -2,11 +2,58 @@ import argparse
 
 
 def part_1(input_string):
-    pass
+    components = list(map(lambda p: tuple(p.split('/')), input_string.split('\n')))
+    bridges = [(0, '0')]
+    max_strength = 0
+    while bridges:
+        new_bridges = []
+        strength, bridge = bridges.pop(0)
+        bridge_components = bridge.split('/')
+        last_bridge_component = ''
+        for bc in bridge_components:
+            if bridge_components.count(bc) % 2:
+                last_bridge_component = bc
+                break
+        for component in components:
+            if last_bridge_component in component:
+                bridge_component = '/'.join(component)
+                bridge_component_strength = int(component[0]) + int(component[1])
+                if bridge_component not in bridge:
+                    new_bridges.append((strength + bridge_component_strength, bridge + '/' + bridge_component))
+        bridges.extend(new_bridges)
+        bridges.sort(key=lambda b: b[0], reverse=True)
+        if bridges:
+            max_strength = max(max_strength, bridges[0][0])
+    print(max_strength)
 
 
 def part_2(input_string):
-    pass
+    components = list(map(lambda p: tuple(p.split('/')), input_string.split('\n')))
+    bridges = [(0, '0')]
+    max_length = 0
+    max_strength = 0
+    while bridges:
+        new_bridges = []
+        strength, bridge = bridges.pop(0)
+        bridge_components = bridge.split('/')
+        last_bridge_component = ''
+        for bc in bridge_components:
+            if bridge_components.count(bc) % 2:
+                last_bridge_component = bc
+                break
+        for component in components:
+            if last_bridge_component in component:
+                bridge_component = '/'.join(component)
+                bridge_component_strength = int(component[0]) + int(component[1])
+                if bridge_component not in bridge:
+                    new_bridges.append((strength + bridge_component_strength, bridge + '/' + bridge_component))
+        bridges.extend(new_bridges)
+        bridges.sort(key=lambda b: b[1].count('/'), reverse=True)
+        if bridges:
+            max_length = max(max_length, bridges[0][1].count('/'))
+            if max_length == bridges[0][1].count('/'):
+                max_strength = max(max_strength, bridges[0][0])
+    print(max_strength)
 
 
 def main():
--- 2017/Day_25.py
+++ 2017/Day_25.py
@@ -1,12 +1,84 @@
 import argparse
+import re
 
 
 def part_1(input_string):
-    pass
+    state, steps = re.search(r'Begin in state ([\w]).\nPerform a diagnostic checksum after (\d+) steps.', input_string).groups()
+    steps = int(steps)
+    slots = [0 for _ in range(steps)]
+    cur = 0
+    for _ in range(steps):
+        if state == 'A':
+            if slots[cur] == 0:
+                slots[cur] = 1
+                cur += 1
+                state = 'B'
+                continue
+            if slots[cur] == 1:
+                slots[cur] = 0
+                cur += 1
+                state = 'F'
+                continue
+        if state == 'B':
+            if slots[cur] == 0:
+                # slots[cur] = 1
+                cur -= 1
+                state = 'B'
+                continue
+            if slots[cur] == 1:
+                # slots[cur] = 1
+                cur -= 1
+                state = 'C'
+                continue
+        if state == 'C':
+            if slots[cur] == 0:
+                slots[cur] = 1
+                cur -= 1
+                state = 'D'
+                continue
+            if slots[cur] == 1:
+                slots[cur] = 0
+                cur += 1
+                state = 'C'
+                continue
+        if state == 'D':
+            if slots[cur] == 0:
+                slots[cur] = 1
+                cur -= 1
+                state = 'E'
+                continue
+            if slots[cur] == 1:
+                # slots[cur] = 1
+                cur += 1
+                state = 'A'
+                continue
+        if state == 'E':
+            if slots[cur] == 0:
+                slots[cur] = 1
+                cur -= 1
+                state = 'F'
+                continue
+            if slots[cur] == 1:
+                slots[cur] = 0
+                cur -= 1
+                state = 'D'
+                continue
+        if state == 'F':
+            if slots[cur] == 0:
+                slots[cur] = 1
+                cur += 1
+                state = 'A'
+                continue
+            if slots[cur] == 1:
+                slots[cur] = 0
+                cur -= 1
+                state = 'E'
+                continue
+    print(slots.count(1))
 
 
 def part_2(input_string):
-    pass
+    print("Merry Christmas!")
 
 
 def main():
