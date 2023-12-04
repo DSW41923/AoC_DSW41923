d74fa383191501c5e3499952ec378ac9d4679ca4
Jen-Hsuan (Jackie) Hsiang <dsw041923@gmail.com>
2023-12-04 16:30:21 +0000

    Complete Day 04

--- 2023/Day_04.py
+++ 2023/Day_04.py
@@ -1,12 +1,28 @@
 import argparse
+import re
 
 
 def part_1(input_string):
-    pass
+    result = 0
+    for winning_numbers, holding_numbers in re.findall(r"Card +\d+: ([\d ]+) \| ([\d ]+)", input_string):
+        winning_numbers = set(re.findall(r"(\d+)", winning_numbers))
+        holding_numbers = set(re.findall(r"(\d+)", holding_numbers))
+        winning_numbers_held = len(winning_numbers & holding_numbers)
+        if winning_numbers_held > 0:
+            result += (2**(winning_numbers_held - 1))
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    card_counts = [1 for _ in range(len(input_string.split('\n')))]
+    for card_id, winning_numbers, holding_numbers in re.findall(r"Card +(\d+): ([\d ]+) \| ([\d ]+)", input_string):
+        card_id = int(card_id)
+        winning_numbers = set(re.findall(r"(\d+)", winning_numbers))
+        holding_numbers = set(re.findall(r"(\d+)", holding_numbers))
+        winning_numbers_held = len(winning_numbers & holding_numbers)
+        for i in range(winning_numbers_held):
+            card_counts[card_id+i] += card_counts[card_id-1]
+    print(sum(card_counts))
 
 
 def main():
