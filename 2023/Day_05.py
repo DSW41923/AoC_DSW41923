1efe1f0a73ea260564eee31685cfd8f40ae5c6d7
Jen-Hsuan (Jackie) Hsiang <dsw041923@gmail.com>
2023-12-05 11:53:38 +0000

    Complete Day 05

--- 2023/Day_05.py
+++ 2023/Day_05.py
@@ -1,12 +1,76 @@
 import argparse
+import re
 
 
 def part_1(input_string):
-    pass
+    input_data = input_string.split('\n')
+    seeds_data, maps_data = input_data[0], input_data[2:]
+    seeds = list(map(int, re.findall(r"(\d+)", seeds_data)))
+    maps_data = "\n".join(maps_data).split('\n\n')
+    for map_data in maps_data:
+        mapping = []
+        for dest, source, range_len in re.findall(r"(\d+) (\d+) (\d+)\n?", map_data):
+            dest, source, range_len = tuple(map(int, (dest, source, range_len)))
+            mapping.append((source, source + range_len - 1, dest))
+        mapping.sort(key=lambda m:m[0])
+        for i in range(len(seeds)):
+            for source_low, source_high, dest in mapping:
+                if source_low <= seeds[i] <= source_high:
+                    seeds[i] = dest+(seeds[i]-source_low)
+                    break
+
+    print(min(seeds))
 
 
 def part_2(input_string):
-    pass
+    input_data = input_string.split('\n')
+    seeds_data, maps_data = input_data[0], input_data[2:]
+    seeds = []
+    for seed_start, seed_range in re.findall(r"(\d+) (\d+)", seeds_data):
+        seed_start, seed_range = tuple(map(int, (seed_start, seed_range)))
+        seeds.append((seed_start, seed_start + seed_range - 1))
+    maps_data = "\n".join(maps_data).split('\n\n')
+    for map_data in maps_data:
+        converted = []
+        mapping = []
+        for dest, source, range_len in re.findall(r"(\d+) (\d+) (\d+)\n?", map_data):
+            dest, source, range_len = tuple(map(int, (dest, source, range_len)))
+            mapping.append((source, source + range_len - 1, dest))
+        mapping.sort(key=lambda m:m[0])
+        while seeds:
+            seed_start, seed_end = seeds.pop(0)
+            for mapping_id, mapping_data in enumerate(mapping):
+                source_low, source_high, dest = mapping_data
+                if source_high < seed_start:
+                    if mapping_id == (len(mapping) - 1):
+                        converted.append((seed_start, seed_end))
+                        break
+                    continue
+                if seed_end < source_low:
+                    if mapping_id == 0:
+                        converted.append((seed_start, seed_end))
+                        break
+                elif seed_start < source_low:
+                    converted.append((seed_start, source_low-1))
+                    if seed_end <= source_high:
+                        converted.append((dest, dest+(seed_end-source_low)))
+                        break
+                    else:
+                        converted.append((dest, dest+(source_high-source_low)))
+                        seeds.append((source_high+1, seed_end))
+                        break
+                else:
+                    if seed_end <= source_high:
+                        converted.append((dest+(seed_start-source_low), dest+(seed_end-source_low)))
+                        break
+                    else:
+                        converted.append((dest+(seed_start-source_low), dest+(source_high-source_low)))
+                        seeds.append((source_high+1, seed_end))
+                        break
+        seeds = converted
+
+    seeds.sort(key=lambda m:m[0])
+    print(seeds[0][0])
 
 
 def main():
