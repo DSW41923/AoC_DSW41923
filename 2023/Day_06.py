52254dd40358f3371f3e2d5e23030447614ae021
Jen-Hsuan (Jackie) Hsiang <dsw041923@gmail.com>
2023-12-06 06:04:25 +0000

    Complete Day 06

--- 2023/Day_06.py
+++ 2023/Day_06.py
@@ -1,12 +1,41 @@
 import argparse
+import re
 
 
 def part_1(input_string):
-    pass
+    times, distances = input_string.split('\n')
+    times = list(map(int, re.findall(r"(\d+)", times)))
+    distances = list(map(int, re.findall(r"(\d+)", distances)))
+    races = list(zip(times, distances))
+    result = 1
+    for time, distance in races:
+        time_min, time_max = 0, 0
+        while time_min == time_max == 0:
+            for t in range(1, time):
+                if distance <= t * (time - t):
+                    time_min = t
+                    time_max = time - t
+                    break
+            if time_min == time_max:
+                distance += 1
+        result *= (time_max - time_min + 1)
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    time, distance = tuple(map(lambda x: int(''.join(re.findall(r"(\d+)", x))), input_string.split('\n')))
+    result = 1
+    time_min, time_max = 0, 0
+    while time_min == time_max == 0:
+        for t in range(1, time):
+            if distance <= t * (time - t):
+                time_min = t
+                time_max = time - t
+                break
+        if time_min == time_max:
+            distance += 1
+    result *= (time_max - time_min + 1)
+    print(result)
 
 
 def main():
