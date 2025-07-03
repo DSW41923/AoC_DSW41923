473ccf7b1866eed39a62480c72f5db38c614aa92
DSW41923 <dsw041923@gmail.com>
2025-07-03 14:40:15 +0800

    Complete Day03 and Day04

--- 2024/Day_03.py
+++ 2024/Day_03.py
@@ -1,12 +1,34 @@
 import argparse
+import re
 
 
 def part_1(input_string):
-    pass
+    result = 0
+    for a, b in re.findall(r'mul\((\d+),(\d+)\)',input_string):
+        result += int(a) * int(b)
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    result = 0
+    dos = [d.start(0) for d in re.finditer(r'do\(\)',input_string)]
+    donts = [d.start(0) for d in re.finditer(r'don\'t\(\)',input_string)]
+    for m in re.finditer(r'mul\(\d+,\d+\)',input_string):
+        index = m.start(0)
+        nearest_do = 0
+        for d in dos:
+            if d > index:
+                break
+            nearest_do = d
+        nearest_donts = 0
+        for d in donts:
+            if d > index:
+                break
+            nearest_donts = d
+        if nearest_do > nearest_donts or nearest_do == nearest_donts == 0:
+            a, b = re.findall(r'mul\((\d+),(\d+)\)',m.group(0))[0]
+            result += int(a) * int(b)
+    print(result)
 
 
 def main():
