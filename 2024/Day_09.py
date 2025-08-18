a9bd835b1870e75103f5733c26d2146e7fdf60d3
DSW41923 <dsw041923@gmail.com>
2025-08-18 18:18:00 +0800

    Complete Day 09 10 and update .gitignore

--- 2024/Day_09.py
+++ 2024/Day_09.py
@@ -1,12 +1,82 @@
 import argparse
 
 
+def get_checksum(disk_map):
+    checksum = 0
+    for i in range(len(disk_map)):
+        if disk_map[i] == '.':
+            continue
+        checksum += i * int(disk_map[i])
+    return checksum
+
+
 def part_1(input_string):
-    pass
+    dense_disk_map = list(input_string)
+    disk_map_len = sum(map(int, dense_disk_map))
+    disk_map = ['.' for _ in range(disk_map_len)]
+    pos = 0
+    file_num = 0
+    for i in range(len(dense_disk_map)):
+        if i % 2 == 0:
+            for _ in range(int(dense_disk_map[i])):
+                disk_map[pos] = str(file_num)
+                pos += 1
+            file_num += 1
+        else:
+            pos += int(dense_disk_map[i])
+    start_pos = 0
+    end_pos = disk_map_len-1
+    while end_pos > start_pos:
+        if disk_map[end_pos] != '.':
+            for i in range(start_pos, end_pos):
+                if disk_map[i] == '.':
+                    start_pos = i
+                    break
+            disk_map[start_pos], disk_map[end_pos] = disk_map[end_pos], disk_map[start_pos]
+        end_pos -= 1
+
+    print(get_checksum(disk_map))
 
 
 def part_2(input_string):
-    pass
+    dense_disk_map = list(input_string)
+    disk_map_len = sum(map(int, dense_disk_map))
+    disk_map = ['.' for _ in range(disk_map_len)]
+    files = []
+    spaces = []
+    pos = 0
+    file_num = 0
+    for i in range(len(dense_disk_map)):
+        if i % 2 == 0:
+            file_size = int(dense_disk_map[i])
+            files.append((str(file_num), pos, file_size))
+            for _ in range(file_size):
+                disk_map[pos] = str(file_num)
+                pos += 1
+            file_num += 1
+        else:
+            space_size = int(dense_disk_map[i])
+            if space_size > 0:
+                spaces.append((pos, space_size))
+            pos += space_size
+
+    files.sort(key=lambda f: int(f[0]), reverse=True)
+    for file_id, file_pos, file_size in files:
+        target_pos = file_pos
+        for space_pos, space_size in spaces:
+            if space_size >= file_size:
+                target_pos = space_pos
+                spaces.remove((space_pos, space_size))
+                if space_size > file_size:
+                    spaces.append((space_pos+file_size, space_size-file_size))
+                    spaces.sort(key=lambda s: s[0])
+                break
+        if target_pos < file_pos:
+            for i in range(file_size):
+                disk_map[file_pos+i] = '.'
+                disk_map[target_pos+i] = file_id
+
+    print(get_checksum(disk_map))
 
 
 def main():
--- 2024/Day_10.py
+++ 2024/Day_10.py
@@ -1,12 +1,58 @@
 import argparse
 
 
+def get_map_data(input_string):
+    height_map = list(map(list, input_string.split('\n')))
+    max_x = len(height_map)
+    max_y = len(height_map[0])
+    trailheads = []
+    for x in range(max_x):
+        for y in range(max_y):
+            if height_map[x][y] == '0':
+                trailheads += [(x,y)]
+    return height_map, max_x, max_y, trailheads
+
+
 def part_1(input_string):
-    pass
+    height_map, max_x, max_y, trailheads = get_map_data(input_string)
+    result = 0
+    for trailhead in trailheads:
+        trail_fronts = [trailhead]
+        for i in range(1,10):
+            next_fronts = []
+            for trailhead_x, trailhead_y in trail_fronts:
+                if trailhead_x > 0 and int(height_map[trailhead_x-1][trailhead_y]) == i:
+                    next_fronts.append((trailhead_x-1, trailhead_y))
+                if trailhead_x < max_x-1 and int(height_map[trailhead_x+1][trailhead_y]) == i:
+                    next_fronts.append((trailhead_x+1, trailhead_y))
+                if trailhead_y > 0 and int(height_map[trailhead_x][trailhead_y-1]) == i:
+                    next_fronts.append((trailhead_x, trailhead_y-1))
+                if trailhead_y < max_y-1 and int(height_map[trailhead_x][trailhead_y+1]) == i:
+                    next_fronts.append((trailhead_x, trailhead_y+1))
+            trail_fronts = list(set(next_fronts))
+        result += len(set(trail_fronts))
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    height_map, max_x, max_y, trailheads = get_map_data(input_string)
+    result = 0
+    for trailhead in trailheads:
+        trail_fronts = [trailhead]
+        for i in range(1,10):
+            next_fronts = []
+            for trailhead_x, trailhead_y in trail_fronts:
+                if trailhead_x > 0 and int(height_map[trailhead_x-1][trailhead_y]) == i:
+                    next_fronts.append((trailhead_x-1, trailhead_y))
+                if trailhead_x < max_x-1 and int(height_map[trailhead_x+1][trailhead_y]) == i:
+                    next_fronts.append((trailhead_x+1, trailhead_y))
+                if trailhead_y > 0 and int(height_map[trailhead_x][trailhead_y-1]) == i:
+                    next_fronts.append((trailhead_x, trailhead_y-1))
+                if trailhead_y < max_y-1 and int(height_map[trailhead_x][trailhead_y+1]) == i:
+                    next_fronts.append((trailhead_x, trailhead_y+1))
+            trail_fronts = next_fronts
+        result += len(trail_fronts)
+    print(result)
 
 
 def main():
