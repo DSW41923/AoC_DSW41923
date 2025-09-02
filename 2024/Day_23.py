b4324fd7360f58ca4ab7b57e912d1cdf5a274eb4
DSW41923 <dsw041923@gmail.com>
2025-09-02 16:56:14 +0800

    Complete Day 22 23 24 25

--- 2024/Day_23.py
+++ 2024/Day_23.py
@@ -1,12 +1,59 @@
 import argparse
+import re
+
+from itertools import combinations
+
+
+def get_connections_data(input_string):
+    connections = {}
+    for computer_0, computer_1 in re.findall(r"(\w+)\-(\w+)", input_string):
+        if computer_0 not in connections:
+            connections[computer_0] = []
+        if computer_1 not in connections:
+            connections[computer_1] = []
+        connections[computer_0].append(computer_1)
+        connections[computer_1].append(computer_0)
+    return connections
 
 
 def part_1(input_string):
-    pass
+    connections = get_connections_data(input_string)
+    result = 0
+    for c0, c1, c2 in combinations(connections.keys(), 3):
+        if c0 in connections[c1] and c1 in connections[c2] and c2 in connections[c0]:
+            if any([c.startswith('t') for c in (c0, c1, c2)]):
+                result += 1
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    connections = get_connections_data(input_string)
+    interconnected = []
+    for c0, c1, c2 in combinations(connections.keys(), 3):
+        if c0 in connections[c1] and c1 in connections[c2] and c2 in connections[c0]:
+            interconnected.append((c0, c1, c2))
+    full_interconnections = set()
+    for c0, c1, c2 in interconnected:
+        if any([c0 in c and c1 in c and c2 in c for c in full_interconnections]):
+            continue
+        interconnections = set([(c0, c1, c2)])
+        while interconnections:
+            icc = interconnections.pop()
+            interconnected_candidates = set(connections[icc[0]])
+            for c in icc[1:]:
+                interconnected_candidates = interconnected_candidates.intersection(set(connections[c]))
+            if len(interconnected_candidates) == 0:
+                full_interconnections.add(icc)
+            for c in interconnected_candidates:
+                new_icc = tuple(list(icc)+[c])
+                if not any([set(new_icc).issubset(ic) for ic in interconnections]):
+                    interconnections.add(new_icc)
+    full_interconnections = list(full_interconnections)
+    full_interconnections.sort(key=lambda i: len(i), reverse=True)
+    full_interconnection_computers = list(full_interconnections[0])
+    full_interconnection_computers.sort()
+    print(','.join(full_interconnection_computers))
+
 
 
 def main():
--- 2024/Day_25.py
+++ 2024/Day_25.py
@@ -2,11 +2,36 @@ import argparse
 
 
 def part_1(input_string):
-    pass
+    lock_pads = []
+    key_pads = []
+    for pad in input_string.split('\n\n'):
+        pad = list(map(list, pad.split('\n')))
+        pad_type = None
+        if all([p == '#' for p in pad[0]]):
+            pad_type = 'LOCK'
+            pad = pad[1:]
+        elif all([p == '#' for p in pad[-1]]):
+            pad_type = 'KEY'
+            pad = pad[:-1]
+
+        pad_columns = [[pad[i][j] for i in range(6)] for j in range(5)]
+        pad_numeric = tuple(map(lambda c: c.count('#'), pad_columns))
+        if pad_type == 'LOCK':
+            lock_pads.append(pad_numeric)
+        elif pad_type == 'KEY':
+            key_pads.append(pad_numeric)
+
+    result = 0
+    for lock in lock_pads:
+        for key in key_pads:
+            fitting = tuple(lock[i]+key[i] for i in range(5))
+            if all([f <= 5 for f in fitting]):
+                result += 1
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    print("Merry Christmas!")
 
 
 def main():
