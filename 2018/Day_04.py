217e827e60bda1732bbc0e3effdd725a1d1ac38c
DSW41923 <dsw041923@gmail.com>
2025-09-06 18:39:30 +0800

    Complete Day 03 04 05 06

--- 2018/Day_04.py
+++ 2018/Day_04.py
@@ -1,12 +1,49 @@
 import argparse
+import re
+
+from collections import Counter
+
+
+def get_guard_data(input_string):
+    records = []
+    for date, time, detail in re.findall(r"\[([\d\-]+) ([\d:]+)\] ([\d\w# ]+)", input_string):
+        records.append((date, time, detail))
+    records.sort()
+    guards = {}
+    cur_guard_id = ''
+    asleep = 0
+    for date, time, detail in records:
+        if "shift" in detail:
+            cur_guard_id = re.search(r"Guard #(\d+)", detail).groups()[0]
+            asleep = 0
+        elif "asleep" in detail:
+            asleep = int(time.split(':')[-1])
+        elif "wake" in detail:
+            if cur_guard_id not in guards:
+                guards[cur_guard_id] = []
+            guards[cur_guard_id].extend(list(range(asleep, int(time.split(':')[-1]))))
+    return guards
 
 
 def part_1(input_string):
-    pass
+    guards = get_guard_data(input_string)
+    max_sleep = max([len(s) for s in guards.values()])
+    for guard in guards:
+        if len(guards[guard]) == max_sleep:
+            sleep_min_count = Counter(guards[guard])
+            print(int(guard) * sleep_min_count.most_common(1)[0][0])
+            break
 
 
 def part_2(input_string):
-    pass
+    guards = get_guard_data(input_string)
+    max_sleep_freq = max([Counter(guards[guard]).most_common(1)[0][1] for guard in guards])
+    for guard in guards:
+        sleep_min_count = Counter(guards[guard])
+        most_sleep_freq = sleep_min_count.most_common(1)[0][1]
+        if most_sleep_freq == max_sleep_freq:
+            print(int(guard) * sleep_min_count.most_common(1)[0][0])
+            break
 
 
 def main():
--- 2018/Day_05.py
+++ 2018/Day_05.py
@@ -1,12 +1,39 @@
 import argparse
 
+from string import ascii_lowercase, ascii_uppercase
+
+
+def get_triggered_units():
+    return [a+b for a, b in (list(zip(ascii_lowercase, ascii_uppercase)) + list(zip(ascii_uppercase, ascii_lowercase)))]
+
+
+def react(polymer, triggered_units):
+    while True:
+        new_polymer = polymer
+        for unit in triggered_units:
+            new_polymer = new_polymer.replace(unit, '')
+        if new_polymer == polymer:
+            break
+        polymer = new_polymer
+    return polymer
+
 
 def part_1(input_string):
-    pass
+    polymer = input_string
+    triggered_units = get_triggered_units()
+    polymer = react(polymer, triggered_units)
+    print(len(polymer))
 
 
 def part_2(input_string):
-    pass
+    polymer = input_string
+    triggered_units = get_triggered_units()
+    min_polymer_len = len(polymer)
+    for lower, upper in zip(ascii_lowercase, ascii_uppercase):
+        trial_polymer = polymer.replace(lower, '').replace(upper, '')
+        trial_polymer = react(trial_polymer, triggered_units)
+        min_polymer_len = min(len(trial_polymer), min_polymer_len)
+    print(min_polymer_len)
 
 
 def main():
