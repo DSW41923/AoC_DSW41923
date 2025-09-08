58c08b5a4ea0624ae661aeaf7119e6038262cbc0
DSW41923 <dsw041923@gmail.com>
2025-09-08 20:30:24 +0800

    Complete Day 07 08

--- 2018/Day_07.py
+++ 2018/Day_07.py
@@ -1,12 +1,83 @@
 import argparse
+import re
+
+from string import ascii_uppercase
+
+
+def get_steps_data(input_string):
+    steps_order = {}
+    all_steps = set()
+    for step_a, step_b in re.findall(r"Step (\w) must be finished before step (\w) can begin.", input_string):
+        if step_b not in steps_order:
+            steps_order[step_b] = []
+        steps_order[step_b].append(step_a)
+        all_steps.add(step_a)
+        all_steps.add(step_b)
+    return steps_order, all_steps
 
 
 def part_1(input_string):
-    pass
+    steps_order, all_steps = get_steps_data(input_string)
+
+    traversed = []
+    next_steps = []
+    while all_steps:
+        if not traversed:
+            for step in all_steps:
+                if step not in steps_order:
+                    next_steps.append(step)
+        for step, pre in steps_order.items():
+            if all([s in traversed for s in pre]) and step not in traversed:
+                next_steps.append(step)
+        next_steps = list(set(next_steps))
+        next_steps.sort()
+        next_step = next_steps[0]
+        all_steps.remove(next_step)
+        traversed.append(next_step)
+        next_steps = next_steps[1:]
+
+    print(''.join(traversed))
 
 
 def part_2(input_string):
-    pass
+    steps_order, all_steps = get_steps_data(input_string)
+
+    traversed = []
+    workers = [('', 0) for _ in range(5)]
+    next_steps = []
+    seconds = 0
+    while all_steps:
+        if any([worker[1] != 0 for worker in workers]):
+            for i in range(5):
+                if workers[i][1] > 1:
+                    workers[i] = (workers[i][0], workers[i][1]-1)
+                elif workers[i][1] == 1:
+                    traversed.append(workers[i][0])
+                    workers[i] = ('', 0)
+
+        if any([worker[1] == 0 for worker in workers]):
+            if not traversed:
+                if all([worker[0] == '' for worker in workers]):
+                    for step in all_steps:
+                        if step not in steps_order:
+                            next_steps.append(step)
+            for step, pre in steps_order.items():
+                if all([s in traversed for s in pre]) and step not in traversed and step not in [worker[0] for worker in workers]:
+                    next_steps.append(step)
+            next_steps = list(set(next_steps))
+            for next_step in next_steps:
+                for i in range(5):
+                    if workers[i][0] == '':
+                        workers[i] = (next_step, 61+ascii_uppercase.index(next_step))
+                        all_steps.remove(next_step)
+                        break
+            for worker in workers:
+                if worker[0] in next_steps:
+                    next_steps.remove(worker[0])
+
+        seconds += 1
+
+    print(seconds+max([worker[1] for worker in workers])-1)
 
 
 def main():
--- 2018/Day_08.py
+++ 2018/Day_08.py
@@ -1,12 +1,104 @@
 import argparse
 
 
+# def parse_tree_data(data, sub_tree_count):
+#     if len(data) == 0:
+#         return 0, 0
+#     if sub_tree_count == 0:
+#         child_count = data[0]
+#         metadata_count = data[1]
+#         result = sum(data[2:2+metadata_count])
+#         return 2+metadata_count, result
+#     total_sub_tree_data_len = 0
+#     metadata_count = 0
+#     result = 0
+#     for _ in range(sub_tree_count):
+#         child_count = data[0]
+#         metadata_count = data[1]
+#         sub_tree_data_len, result = parse_tree_data(data[2:], child_count)
+#         total_sub_tree_data_len += sub_tree_data_len
+#         result += sum(data[sub_tree_data_len:sub_tree_data_len+metadata_count])
+#         data = data[sub_tree_data_len:]
+#     return 2+total_sub_tree_data_len+metadata_count, result
+
+
+class Tree:
+    def __init__(self, child_count, metadata_count):
+        self.children = []
+        self.child_count = child_count
+        self.metadata = []
+        self.metadata_count = metadata_count
+        self.value = None
+
+    def __str__(self):
+         return str((self.child_count, len(self.children), self.metadata_count, self.metadata))
+
+    def set_metadata(self, metadata):
+        self.metadata = metadata
+
+    def add_child(self, node):
+        self.children.append(node)
+
+    def add_children(self, nodes):
+        self.children.extend(nodes)
+
+    def evaluate_value(self):
+        if self.child_count == 0:
+            self.value = sum(self.metadata)
+        else:
+            self.value = 0
+            for m in self.metadata:
+                if 0 < m <= self.child_count:
+                    if not self.children[m-1].value:
+                        self.children[m-1].evaluate_value()
+                    self.value += self.children[m-1].value
+
+
 def part_1(input_string):
-    pass
+    data = list(map(int, input_string.split(' ')))
+    result = 0
+    trees = [(data[0], data[1])]
+    cur = 2
+    while cur < len(data):
+        if trees[-1][0] > 0:
+            child_count = data[cur+0]
+            metadata_count = data[cur+1]
+            trees.append((child_count, metadata_count))
+            cur += 2
+        else:
+            child_count, metadata_count = trees.pop()
+            result += sum(data[cur:cur+metadata_count])
+            if trees:
+                trees[-1] = (trees[-1][0]-1, trees[-1][1])
+            cur += metadata_count
+
+    print(result)
 
 
 def part_2(input_string):
-    pass
+    data = list(map(int, input_string.split(' ')))
+    nodes = [(Tree(data[0], data[1]), data[0], data[1])]
+    root = None
+    cur = 2
+    while cur < len(data):
+        if nodes[-1][1] > 0:
+            child_count = data[cur+0]
+            metadata_count = data[cur+1]
+            nodes.append((Tree(child_count, metadata_count), child_count, metadata_count))
+            cur += 2
+        else:
+            node, child_count, metadata_count = nodes.pop()
+            metadata = data[cur:cur+metadata_count]
+            node.set_metadata(metadata)
+            if nodes:
+                nodes[-1][0].add_child(node)
+                nodes[-1] = (nodes[-1][0], nodes[-1][1]-1, nodes[-1][2])
+            else:
+                root = node
+            cur += metadata_count
+
+    root.evaluate_value()
+    print(root.value)
 
 
 def main():
