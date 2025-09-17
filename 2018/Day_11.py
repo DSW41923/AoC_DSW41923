1e3e53a339828dd99a4fc20e24b46bbffb1195b8
DSW41923 <dsw041923@gmail.com>
2025-09-17 13:31:50 +0800

    Complete Day 11 12

--- 2018/Day_11.py
+++ 2018/Day_11.py
@@ -1,12 +1,52 @@
 import argparse
 
 
+def get_cells_data(input_string):
+    grid_serial_number = int(input_string)
+    cells = [[0 for _ in range(301)] for _ in range(301)]
+    for x in range(1, 301):
+        for y in range(1, 301):
+            rack_id = x + 10
+            power_level = rack_id * y
+            power_level += grid_serial_number
+            power_level *= rack_id
+            power_level = int(str(power_level)[-3])
+            power_level -= 5
+            cells[y][x] = power_level
+    return cells
+
+
 def part_1(input_string):
-    pass
+    cells = get_cells_data(input_string)
+    max_power_level = 0
+    result = None
+    for x in range(1, 299):
+        for y in range(1, 299):
+            total_power_level = sum(cells[y][x:x+3]) + sum(cells[y+1][x:x+3]) + sum(cells[y+2][x:x+3])
+            if total_power_level > max_power_level:
+                max_power_level = total_power_level
+                result = (x,y)
+    print(','.join(list(map(str, result))))
 
 
 def part_2(input_string):
-    pass
+    cells = get_cells_data(input_string)
+    max_power_level = 0
+    result = None
+    for size in range(1, 301):
+        temp_max = 0
+        for x in range(1, 302-size):
+            for y in range(1, 302-size):
+                total_power_level = 0
+                for i in range(size):
+                    total_power_level += sum(cells[y+i][x:x+size])
+                temp_max = max(temp_max, total_power_level)
+                if total_power_level > max_power_level:
+                    max_power_level = total_power_level
+                    result = (x,y,size)
+        if temp_max == 0:
+            break
+    print(','.join(list(map(str, result))))
 
 
 def main():
