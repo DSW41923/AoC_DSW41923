4320826fafa928e2ed249df0b9ad4b251d28654c
DSW41923 <dsw041923@gmail.com>
2025-09-20 14:48:08 +0800

    Complete Day 13 14 15 16

--- 2018/Day_14.py
+++ 2018/Day_14.py
@@ -2,11 +2,31 @@ import argparse
 
 
 def part_1(input_string):
-    pass
+    recipes_number = int(input_string)
+    recipes = [3, 7]
+    cur_0, cur_1 = 0, 1
+    while len(recipes) < (recipes_number+10):
+        new_recipes = recipes[cur_0] + recipes[cur_1]
+        recipes.extend(list(map(int, list(str(new_recipes)))))
+        cur_0 += (1+recipes[cur_0])
+        cur_0 %= len(recipes)
+        cur_1 += (1+recipes[cur_1])
+        cur_1 %= len(recipes)
+    print(''.join(list(map(str, recipes[recipes_number:recipes_number+10]))))
 
 
 def part_2(input_string):
-    pass
+    recipes = "37"
+    cur_0, cur_1 = 0, 1
+    while input_string not in [recipes[-len(input_string)-1:-1], recipes[-len(input_string):]]:
+        cur_0_recipe, cur_1_recipe = int(recipes[cur_0]), int(recipes[cur_1])
+        new_recipes = cur_0_recipe + cur_1_recipe
+        recipes += str(new_recipes)
+        cur_0 += (1+cur_0_recipe)
+        cur_0 %= len(recipes)
+        cur_1 += (1+cur_1_recipe)
+        cur_1 %= len(recipes)
+    print(recipes.index(input_string))
 
 
 def main():
