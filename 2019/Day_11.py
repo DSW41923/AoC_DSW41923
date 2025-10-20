576d5192ef904922c86d8d522ebd6700d47391f0
DSW41923 <dsw041923@gmail.com>
2025-10-20 22:12:42 +0800

    Init 2019 AoC

---
+++ 2019/Day_11.py
@@ -0,0 +1,32 @@
+import argparse
+
+
+def part_1(input_string):
+    pass
+
+
+def part_2(input_string):
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--part",
+                        help="Specify puzzle 1 or puzzle 2 to be solved. Run both by default.",
+                        required=False)
+    args = parser.parse_args()
+    file_input = open('inputs/2019/Input_11.txt', 'r')
+    input_string = file_input.read()
+    file_input.close()
+
+    if args.part == '1':
+        part_1(input_string)
+    elif args.part == '2':
+        part_2(input_string)
+    else:
+        part_1(input_string)
+        part_2(input_string)
+
+
+if __name__ == "__main__":
+    main()
