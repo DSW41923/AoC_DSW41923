9c1142587c9d2b5ad9f38bd1d1304013b43a576e
DSW41923 <dsw041923@gmail.com>
2025-10-26 22:16:45 +0800

    Complete Day 08 09 10

--- 2019/Day_08.py
+++ 2019/Day_08.py
@@ -2,11 +2,44 @@ import argparse
 
 
 def part_1(input_string):
-    pass
+    img_w = 25
+    img_h = 6
+    img_bit = img_w * img_h
+    min_layer = None
+    min_count = img_bit
+    for i in range(len(input_string)//img_bit):
+        cur_layer = list(input_string[i*img_bit:(i+1)*img_bit])
+        cur_layer_count = cur_layer.count('0')
+        if cur_layer_count < min_count:
+            min_layer = cur_layer
+            min_count = cur_layer_count
+    print(min_layer.count('1')*min_layer.count('2'))
 
 
 def part_2(input_string):
-    pass
+    img_w = 25
+    img_h = 6
+    img_bit = img_w * img_h
+    layers = []
+    for i in range(len(input_string)//img_bit):
+        layers.append(list(input_string[i*img_bit:(i+1)*img_bit]))
+    img = [[-1 for _ in range(img_w)] for _ in range(img_h)]
+    for h in range(img_h):
+        for w in range(img_w):
+            pixel_id = h*img_w+w
+            pixels = [l[pixel_id] for l in layers]
+            for p in pixels:
+                if p == '2':
+                    continue
+                if p == '0':
+                    img[h][w] = '#'
+                    break
+                if p == '1':
+                    img[h][w] = '.'
+                    break
+            if img[h][w] not in ['#', '.']:
+                print(h, w, pixel_id, pixels)
+    print('\n'.join(list(map(lambda i:''.join(i), img))))
 
 
 def main():
--- 2019/Day_10.py
+++ 2019/Day_10.py
@@ -1,12 +1,86 @@
 import argparse
 
+from math import inf, gcd
+
+
+def parse_data(input_string):
+    region_map = list(map(list, input_string.split('\n')))
+    y_range = len(region_map)
+    x_range = len(region_map[0])
+    asteroids = []
+    for y in range(y_range):
+        for x in range(x_range):
+            if region_map[y][x] == '#':
+                asteroids.append((y, x))
+    return asteroids
+
+
+def get_monitor_location(asteroids):
+    max_count = 0
+    monitor_location = None
+    for a in asteroids:
+        vectors = set()
+        for o in asteroids:
+            if o != a:
+                vec = (o[0]-a[0], o[1]-a[1])
+                while gcd(abs(vec[0]), abs(vec[1])) > 1:
+                    vec_gcd = gcd(abs(vec[0]), abs(vec[1]))
+                    vec = (vec[0]//vec_gcd, vec[1]//vec_gcd)
+                vectors.add(vec)
+        vectors_count = len(vectors)
+        if vectors_count > max_count:
+            max_count = vectors_count
+            monitor_location = a
+    return max_count, monitor_location
+
+
+def manhatton_distance(p_0, p_1):
+    return sum([abs(a - b) for a, b in zip(p_0, p_1)])
+
 
 def part_1(input_string):
-    pass
+    asteroids = parse_data(input_string)
+    max_count, _ = get_monitor_location(asteroids)
+    print(max_count)
 
 
 def part_2(input_string):
-    pass
+    asteroids = parse_data(input_string)
+    _, monitor_location = get_monitor_location(asteroids)
+    asteroids_direction_r = []
+    asteroids_direction_l = []
+    for a in asteroids:
+        if a != monitor_location:
+            vec = (a[0]-monitor_location[0], a[1]-monitor_location[1])
+            while gcd(abs(vec[0]), abs(vec[1])) > 1:
+                vec_gcd = gcd(abs(vec[0]), abs(vec[1]))
+                vec = (vec[0]//vec_gcd, vec[1]//vec_gcd)
+            if vec[1] > 0 or (vec[0] < 0 and vec[1] == 0):
+                asteroids_direction_r.append((a, vec))
+            if vec[1] < 0 or (vec[0] > 0 and vec[1] == 0):
+                asteroids_direction_l.append((a, vec))
+    asteroids_direction_r.sort(key=lambda ad: -ad[1][0]/((ad[1][0]**2+ad[1][1]**2)**0.5), reverse=True)
+    asteroids_direction_l.sort(key=lambda ad: -ad[1][0]/((ad[1][0]**2+ad[1][1]**2)**0.5))
+    asteroids_direction = asteroids_direction_r + asteroids_direction_l
+    destory_order = [[(asteroids_direction[0])]]
+    for a, v in asteroids_direction[1:]:
+        if destory_order[-1][-1][1] == v:
+            destory_order[-1].append((a, v))
+            continue
+        destory_order.append([(a, v)])
+    for d in destory_order:
+        if len(d) > 1:
+            d.sort(key=lambda d: manhatton_distance(d[0], monitor_location))
+    destory_count = 0
+    while destory_count < 200:
+        for d in destory_order:
+            destoryed = None
+            if len(d) > 0:
+                destoryed = d.pop(0)
+                destory_count += 1
+            if destoryed and destory_count == 200:
+                print(destoryed[0][1]*100+destoryed[0][0])
+                break
 
 
 def main():
