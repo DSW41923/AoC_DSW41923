a809bd0695371aa113e518794a5ff6922461ad79
DSW41923 <dsw041923@gmail.com>
2025-10-31 00:42:10 +0800

    Complete Day 21 22 23 24 25

--- 2019/Day_21.py
+++ 2019/Day_21.py
@@ -1,12 +1,109 @@
 import argparse
 
+from Day_13 import IntCodeComputer
+from itertools import product
+
 
 def part_1(input_string):
-    pass
-
+    instructions = ["AND", "OR", "NOT"]
+    writable_registers = ["T", "J"]
+    read_only_registers = ["A", "B", "C", "D"]
+    register_pairs = [' '.join(rp) for rp in product(read_only_registers+writable_registers, writable_registers) if rp[0] != rp[1]]
+    all_commands = [' '.join(c)+'\n' for c in product(instructions, register_pairs)]
+    all_commands.append("NOT T T\n")
+    all_commands.append("NOT J J\n")
+    program = ""
+    result = 0
+    while result <= 255:
+        '''
+        NOT C J 
+        AND D J 
+        NOT A T 
+        OR T J
+        '''
+        print("Current program: \n"+program, end='')
+        print("Current commands options: ")
+        for i, c in enumerate(all_commands):
+            if c not in program:
+                print(str(i)+") " + c, end='')
+        command_chosen = int(input("Choose desired command to add: "))
+        command = all_commands[command_chosen]
+        instruction = ''.join([program, command, "WALK\n"])
+        computer = IntCodeComputer(input_string)
+        for i in instruction:
+            computer.add_input(ord(i))
+        computer.run()
+        outputs = computer.get_outputs()
+        result = outputs[-1]
+        for o in outputs:
+            if o <= 255:
+                print(chr(o), end='')
+        if result > 255:
+            break
+        else:
+            save_command = input("Save command? (y/n): ")
+            if save_command == 'y':
+                program += command
+    print(result)
 
 def part_2(input_string):
-    pass
+    instructions = ["AND", "OR", "NOT"]
+    writable_registers = ["T", "J"]
+    read_only_registers = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+    register_pairs = [' '.join(rp) for rp in product(read_only_registers+writable_registers, writable_registers) if rp[0] != rp[1]]
+    all_commands = [' '.join(c)+'\n' for c in product(instructions, register_pairs)]
+    all_commands.append("NOT T T\n")
+    all_commands.append("NOT J J\n")
+    program = ""
+    result = 0
+    while result <= 255:
+        '''
+        NOT A J 
+        NOT C T 
+        AND D T 
+        AND H T 
+        OR T J
+        NOT B T 
+        AND D T 
+        OR T J
+        '''
+        command = ''
+        action = int(input("Choose action: 1) Add new command 2) Remove command: "))
+        if action == 1:
+            print("Current commands options: ")
+            for i, c in enumerate(all_commands):
+                print(str(i)+") " + c, end='')
+            command_chosen = int(input("Choose desired command to add: "))
+            if command_chosen < len(all_commands):
+                command = all_commands[command_chosen]
+        elif action == 2:
+            print("Current program: \n"+program, end='')
+            program = program.split('\n')[:-1]
+            for i, c in enumerate(program):
+                print(str(i)+") " + c)
+            command_chosen = int(input("Choose desired command to remove: "))
+            if command_chosen < len(program):
+                program.pop(command_chosen)
+            program = '\n'.join(program)
+            if len(program) > 0:
+                program += '\n'
+        instruction = ''.join([program, command, "RUN\n"])
+        computer = IntCodeComputer(input_string)
+        for i in instruction:
+            computer.add_input(ord(i))
+        computer.run()
+        outputs = computer.get_outputs()
+        result = outputs[-1]
+        for o in outputs:
+            if o <= 255:
+                print(chr(o), end='')
+        if result > 255:
+            break
+        else:
+            save_command = input("Save command? (y/n): ")
+            if save_command == 'y':
+                program += command
+    print(result)
 
 
 def main():
--- 2019/Day_25.py
+++ 2019/Day_25.py
@@ -1,12 +1,68 @@
 import argparse
 
+from Day_13 import IntCodeComputer
+
 
 def part_1(input_string):
-    pass
+    computer = IntCodeComputer(input_string)
+    computer.run()
+    outputs = computer.get_outputs()
+    for o in outputs:
+        print(chr(o), end='')
+    outputs = computer.clear_outputs()
+    
+    while True:
+        '''
+        easter egg
+        space heater
+        sand
+        mug
+        '''
+        action = input("Choose one action: 0) Exit 1) Move 2) Take 3) Drop 4) Inventory ")
+        if action not in ["0", "1", "2", "3", "4"]:
+            continue
+        action = int(action)
+        command = ""
+        if action == 0:
+            break
+        if action == 1:
+            direction = input("Choose direction from NEWS: ")
+            direction = direction.lower()
+            if direction in ['n', "north"]:
+                command = "north\n"
+            elif direction in ['e', "east"]:
+                command = "east\n"
+            elif direction in ['w', "west"]:
+                command = "west\n"
+            elif direction in ['s', "south"]:
+                command = "south\n"
+            else:
+                print("Invalid direction!!")
+                continue
+        elif action == 2:
+            item = input("Choose one item to take: ")
+            command = "take " + item + "\n"
+        elif action == 3:
+            item = input("Choose one item to drop: ")
+            command = "drop " + item + "\n"
+        elif action == 4:
+            command = "inv\n"
+
+        for i in command:
+            computer.add_input(ord(i))
+
+        computer.run()
+        outputs = computer.get_outputs()
+        for o in outputs:
+            if o <= 255:
+                print(chr(o), end='')
+            else:
+                print(o)
+        outputs = computer.clear_outputs()
 
 
 def part_2(input_string):
-    pass
+    print("Merry Christmas!")
 
 
 def main():
